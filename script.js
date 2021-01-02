c2994f44e59488ee8cd70e205837840b077d648c
Santi <70606628+santiareh@users.noreply.github.com>
2021-01-02 10:15:28 -0600

    almost done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // TODO: add code here
 window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-     console.log(response);
-    });
+        return response.json();
+    }).then(function(response) {
+            let container = document.querySelector("#container");
+            for(data of response){
+                let skills = '';
+                for(skill of data.skills){
+                    skills += `${skill},`
+                }
+                container.innerHTML += `<div class="astronaut" style="display: block; background: aliceblue; margin: 10px"> 
+                ${data.firstName} ${data.lastName} 
+                <img class="avatar" src="${data.picture}"
+                <ul>
+                <li>Active:${data.active}</li>
+                <li>Hours in Space:${data.hoursInSpace}</li>
+                <li>${skills}</li></ul>
+                </div>`;
+            };
+        });       
 });
